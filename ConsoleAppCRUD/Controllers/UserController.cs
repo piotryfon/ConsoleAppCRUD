f8b33b42b256ed03d24430f49e59d312aa49890a
piotryfon <31383071+piotryfon@users.noreply.github.com>
2022-02-18 18:52:29 +0100

    -Add while loop end exit

--- ConsoleAppCRUD/Controllers/UserController.cs
+++ ConsoleAppCRUD/Controllers/UserController.cs
@@ -21,23 +21,32 @@ namespace ConsoleAppCRUD.Controllers
             var insertedUser = _dbContext.Add(user);
             if (insertedUser != null)
                 _dbContext.SaveChanges();
+                Console.WriteLine("User added");
         }
 
-        public void UpdateUser(int id, User modifiedUser)
+        public void EditUser(int id, User modifiedUser)
         {
-            User user =  _dbContext.Users.SingleOrDefault(d => d.Id == id);
-            if (user == null)
+            User user = _dbContext.Users.SingleOrDefault(d => d.Id == id);
+            if(user == null)
             {
-                Console.WriteLine("User not found.");
+                Console.WriteLine("User not found");
             }
             else
             {
-                user.Name = modifiedUser.Name;
-                user.EmailAddress = modifiedUser.EmailAddress;
-                int save = _dbContext.SaveChanges();
-                if (save > 0)
-                    Console.WriteLine("User changed");
+                try
+                {
+                    user.Name = modifiedUser.Name;
+                    user.EmailAddress = modifiedUser.EmailAddress;
+                    int save = _dbContext.SaveChanges();
+                    if (save > 0)
+                        Console.WriteLine("User changed");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
+          
         }
 
         public void DeleteUser(int id)
@@ -63,6 +72,15 @@ namespace ConsoleAppCRUD.Controllers
         public IEnumerable<User> GetAllUsers()
         {
             return _dbContext.Users.ToList();
+        }
+
+        public void GetUserById(int Id)
+        {
+            var userById = _dbContext.Users.FirstOrDefault(u => u.Id == Id);
+
+            if(userById != default)
+                Console.WriteLine($"{userById.Name} {userById.EmailAddress}");
+            else Console.WriteLine("User not found");
         }
     }
 }
--- ConsoleAppCRUD/Program.cs
+++ ConsoleAppCRUD/Program.cs
@@ -12,29 +12,80 @@ namespace ConsoleAppCRUD
         static void Main(string[] args)
         {
             Console.WriteLine("CosoleApp CRUD");
+            Console.WriteLine("Select operation");
+            Console.WriteLine("1 Display all users");
+            Console.WriteLine("2 Display user by id");
+            Console.WriteLine("3 Add user");
+            Console.WriteLine("4 Edit user");
+            Console.WriteLine("5 Delete user");
+            Console.WriteLine("6 Exit");
+           
             ApplicationDbContext dbContext = new ApplicationDbContext();
 
             UserController userController = new UserController(dbContext);
 
-            //userController.AddUser(new User()
-            //{
-            //  Name = "John Doe",
-            //  EmailAddress = "johndoe@gmail.com"
-            //});
-
-            userController.UpdateUser(222, new User()
+            string value = Console.ReadLine();
+            while (true)
             {
-                Name = "Joe",
-                EmailAddress = "joe123@yachoo.com"
-            });
+                if (value == "1")
+                {
+                    var users = userController.GetAllUsers();
+                    var allUsers = users.Select(u => $"{u.Id} {u.Name}, {u.EmailAddress}");
+                    foreach (var user in allUsers)
+                    {
+                        Console.WriteLine(string.Join(", ", user));
+                    }
+                }
+                else if (value == "2")
+                {
+                    Console.WriteLine("Insert id");
+                    int idToGetUser = Convert.ToInt32(Console.ReadLine());
+                    userController.GetUserById(idToGetUser);
+                }
+                else if (value == "3")
+                {
+                    Console.WriteLine("Insert user name");
+                    string userName = Console.ReadLine();
+                    Console.WriteLine("Insert user email");
+                    string userEmailAddress = Console.ReadLine();
+                    userController.AddUser(new User()
+                    {
+                        Name = userName,
+                        EmailAddress = userEmailAddress,
+                    });
+                }
+                else if (value == "4")
+                {
+                    Console.WriteLine("Insert user Id");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Insert user name");
+                    string userName = Console.ReadLine();
+                    Console.WriteLine("Insert user email");
+                    string userEmailAddress = Console.ReadLine();
 
-            //userController.DeleteUser(8);
-
-            var users = userController.GetAllUsers();
-            var allUsers = users.Select(u => $"{u.Name}, {u.EmailAddress}");
-            foreach (var user in allUsers)
-            {
-                Console.WriteLine(string.Join(", ", user));
+                    userController.EditUser(id, new User()
+                    {
+                        Name = userName,
+                        EmailAddress = userEmailAddress
+                    });
+                }
+                else if (value == "5")
+                {
+                    Console.WriteLine("Insert user Id");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    userController.DeleteUser(id);
+                }
+                else if (value == "6")
+                {
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid value");
+                }
+                Console.WriteLine("====================");
+                Console.WriteLine("Select operation");
+                value = Console.ReadLine();
             }
         }
     }
